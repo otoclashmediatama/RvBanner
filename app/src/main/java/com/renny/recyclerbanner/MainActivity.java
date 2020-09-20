e8e526fbdd526acd841204a08eb04308483c0fc1
otoclashmediatama <39687903+otoclashmediatama@users.noreply.github.com>
2020-09-21 02:56:58 +0800

    Add files via upload
    This Sample Implementation

---
+++ app/src/main/java/com/renny/recyclerbanner/MainActivity.java
@@ -0,0 +1,63 @@
+package com.renny.recyclerbanner;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.library.swrevo.rvbanner.BannerLayout;
+import com.renny.recyclerbanner.adapter.WebBannerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements BannerLayout.OnBannerItemClickListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BannerLayout  recyclerBanner =  findViewById(R.id.recycler);
+        BannerLayout bannerVertical =  findViewById(R.id.recycler_ver);
+
+        List<String> list = new ArrayList<>();
+        list.add("http://img0.imgtn.bdimg.com/it/u=1352823040,1166166164&fm=27&gp=0.jpg");
+        list.add("http://img3.imgtn.bdimg.com/it/u=2293177440,3125900197&fm=27&gp=0.jpg");
+        list.add("http://img3.imgtn.bdimg.com/it/u=3967183915,4078698000&fm=27&gp=0.jpg");
+        list.add("http://img0.imgtn.bdimg.com/it/u=3184221534,2238244948&fm=27&gp=0.jpg");
+        list.add("http://img4.imgtn.bdimg.com/it/u=1794621527,1964098559&fm=27&gp=0.jpg");
+        list.add("http://img4.imgtn.bdimg.com/it/u=1243617734,335916716&fm=27&gp=0.jpg");
+        WebBannerAdapter webBannerAdapter=new WebBannerAdapter(this,list);
+        webBannerAdapter.setOnBannerItemClickListener(new BannerLayout.OnBannerItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                Toast.makeText(MainActivity.this, "点击了第  " + position+"  项", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        WebBannerAdapter WebBannerAdapter2 =new WebBannerAdapter(this,list);
+        WebBannerAdapter2.setOnBannerItemClickListener(new BannerLayout.OnBannerItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                Toast.makeText(MainActivity.this, "点击了第  " + position+"  项", Toast.LENGTH_SHORT).show();
+            }
+        });
+        recyclerBanner.setAdapter(webBannerAdapter);
+        bannerVertical.setAdapter(WebBannerAdapter2);
+    }
+
+
+    public void jump(View view) {
+        startActivity(new Intent(MainActivity.this, NormalActivity.class));
+    }
+    public void jumpOverFlying(View view) {
+        startActivity(new Intent(MainActivity.this, OverFlyingActivity.class));
+    }
+
+    @Override
+    public void onItemClick(int position) {
+
+    }
+}
---
+++ app/src/main/java/com/renny/recyclerbanner/NormalActivity.java
@@ -0,0 +1,40 @@
+package com.renny.recyclerbanner;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.library.swrevo.rvbanner.RecyclerViewBannerBase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NormalActivity extends AppCompatActivity {
+    RecyclerViewBannerNormal banner, banner2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        banner = (RecyclerViewBannerNormal) findViewById(R.id.banner);
+        banner2 = (RecyclerViewBannerNormal) findViewById(R.id.banner2);
+        List<String> list = new ArrayList<>();
+        list.add("http://img0.imgtn.bdimg.com/it/u=1352823040,1166166164&fm=27&gp=0.jpg");
+
+        banner.initBannerImageView(list, new RecyclerViewBannerBase.OnBannerItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                Toast.makeText(NormalActivity.this, "clicked:" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+        banner2.initBannerImageView(list, new RecyclerViewBannerBase.OnBannerItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                Toast.makeText(NormalActivity.this, "clicked:" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+}
